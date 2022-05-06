8c485b0db6b03e7c52f9dd49d081f4fb95ed7fdd
Matias <leitnermatias22@gmail.com>
2022-05-06 19:42:12 -0300

    Fix active tabs when logged in

--- src/components/navigation/navbar/LoggedInNavItem.vue
+++ src/components/navigation/navbar/LoggedInNavItem.vue
@@ -8,11 +8,11 @@ const props = defineProps({
 
 
 const navItem = "m-2 w-24 h-10 flex justify-start pl-2 text-center items-center border-l-4 border-blue-200 text-slate-900 hover:justify-start hover:border-orange-400"
-const activeTab = "border-orange-300 flex rounded text-black"
+const activeTab = "m-2 w-24 h-10 flex justify-center pl-2 text-center items-center border-l-4 border-orange-400 text-slate-900"
 </script>
 
 <template>
-<div :class="$route.name === name ? 'border-slate-200 shadow-inner mb-2 rounded' : 'border-slate-100 shadow mb-1 rounded'  ">
-    <router-link :class="[navItem, $route.name === name ? activeTab : '']" :to="path">{{text}}</router-link>
+<div :class="$route.name === name ? 'border-slate-200 shadow-inner mb-2 rounded font-bold' : 'border-slate-100 shadow mb-2 rounded'  ">
+    <router-link :class="[$route.name === name ? activeTab : navItem]" :to="path">{{text}}</router-link>
 </div>
 </template>
