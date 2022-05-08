16aaa34ff81a5eb1893e287498826c904e2c5e45
Matias <leitnermatias22@gmail.com>
2022-05-07 22:18:43 -0300

    Fix Sign Up View

---
+++ src/components/signup/SignUpField.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+
+const emit = defineEmits(["update:modelValue"])
+
+const props = defineProps({
+    label: String,
+    type: String,
+    modelValue: String,
+})
+
+function updateValue(value) {
+    emit("update:modelValue", value)
+}
+
+</script>
+
+<template>
+    <div class="flex flex-col w-6/12 m-auto mt-5 text-center">
+        <label for="nickname">{{label}}</label>
+        <input maxlength="20" :value="modelValue" @input="updateValue($event.target.value)" :type="type" name="nickname" class="form-input border-0 border-b-2 border-slate-400 rounded px-5 text-center focus:ring-0 focus:border-blue-200">
+    </div>
+</template>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,20 +1,42 @@
 <script setup>
 import {UserAddIcon} from "@heroicons/vue/outline"
+import SignUpFieldVue from "@/components/signup/SignUpField.vue";
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const signUpData = ref({
+    nick: "",
+    email: "",
+    password: ""
+})
+
+const router = useRouter();
+
+const errors = ref([])
+
+function signUp() {
+    errors.value = [];
+    axios.post("/auth/signup", signUpData.value)
+    .then(response => {
+        router.push({ name: "Login" })
+    })
+    .catch(response => {
+        errors.value.push(response.response.data)
+    })
+}
+
 </script>
 
 <template>
     <div class="flex flex-col w-6/12 m-auto border border-slate-900 rounded shadow-2xl p-5 bg-white">
         <h2 class="m-auto text-2xl text-slate-700 border-b-4 border-slate-200 w-full text-center rounded pb-4 flex justify-center gap-2"><UserAddIcon class="w-5 h-5 self-center"/>Create an account</h2>
-        <div class="flex flex-col w-6/12 m-auto mt-5 text-center">
-            <label for="nickname">Nickname</label>
-            <input maxlength="20" type="text" name="nickname" class="form-input border-0 border-b-2 border-slate-400 rounded px-5 text-center focus:ring-0 focus:border-blue-200">
-        </div>
-        <div class="flex flex-col w-6/12 m-auto mt-5 text-center">
-            <label for="nickname">Password</label>
-            <input maxlength="20" type="password" name="nickname" class="form-input border-0 border-b-2 border-slate-400 rounded px-5 text-center focus:ring-0 focus:border-blue-200">
-        </div>
+        <p class="text-red-600 font-bold mt-2 m-auto" v-for="error, index in errors" :key="index">{{error.message}}</p>
+        <SignUpFieldVue label="Nickname" type="text" v-model="signUpData.nick" />
+        <SignUpFieldVue label="Email" type="email" v-model="signUpData.email" />
+        <SignUpFieldVue label="Password" type="password" v-model="signUpData.password" />
         <div class="flex flex-col w-full rounded m-auto mt-5 text-center">
-            <button type="button" class="border-2 rounded p-2 w-2/12 m-auto mt-5 shadow-lg text-slate-900 bg-blue-200 border-blue-400 hover:bg-opacity-50 hover:border-opacity-0 hover:text-slate-800 hover:shadow-inner">Login</button>
+            <button @click="signUp" type="button" class="border-2 rounded p-2 w-3/12 m-auto mt-5 shadow-lg text-slate-900 bg-blue-200 border-blue-400 hover:bg-opacity-50 hover:border-opacity-0 hover:text-slate-800 hover:shadow-inner">Create Account</button>
         </div>
     </div>
 </template>
