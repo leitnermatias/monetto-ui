4d2fc29ea48c5edc0f683246fed25258e5f7ee07
Matias <leitnermatias22@gmail.com>
2022-05-07 22:30:17 -0300

    Remove imports

--- src/components/navigation/LoggedInNav.vue
+++ src/components/navigation/LoggedInNav.vue
@@ -2,12 +2,6 @@
 import LoggedInNavItemVue from './navbar/LoggedInNavItem.vue';
 import Notifications from './navbar/Notifications.vue';
 import LogoutButton from "./navbar/LogoutButton.vue";
-import { useStore } from 'vuex';
-import { useRouter } from 'vue-router';
-import axios from 'axios';
-
-const store = useStore();
-const router = useRouter();
 
 </script>
 
